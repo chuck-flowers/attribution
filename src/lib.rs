0ca715fd0751d39d03ffc070ec8689848e3add44
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-06-28 07:08:39 -0400

    Does the basic parsing for the macros inputs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::parse_macro_input;
 use syn::AttributeArgs;
 use syn::Item;
 
 #[proc_macro_attribute]
 pub fn attr_args(attr: TokenStream, input: TokenStream) -> TokenStream {
-    unimplemented!()
+    let input_attr = parse_macro_input!(attr as AttributeArgs);
+    let input_struct = parse_macro_input!(input as Item);
+
+    (quote! {
+        #input_struct
+    })
+    .into()
 }
