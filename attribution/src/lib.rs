0f2c0ea89fc1c7eec631ba9f136a12ed99c70bed
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-07-11 23:54:00 -0400

    Exports all the relevant items from the attribution crate

--- attribution/src/lib.rs
+++ attribution/src/lib.rs
@@ -1,1 +1,6 @@
 #![warn(clippy::all)]
+
+pub use attribution_macros::attr_args;
+pub use attribution_types::AttrMap;
+pub use attribution_types::AttrVal;
+pub use attribution_types::TryIntoAttrValError;
