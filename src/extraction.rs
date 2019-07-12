ddd9403c2a4e37f95bcdf6ca7a7524ba0d3e36e8
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-07-11 21:08:09 -0400

    Adds a method that builds the token stream that is used to extract a field value from an AttrMap

---
+++ src/extraction.rs
@@ -0,0 +1,34 @@
+use proc_macro2::Span;
+use proc_macro2::TokenStream;
+use quote::quote;
+use crate::field_spec::FieldSpec;
+
+pub fn build_extractor(field: &FieldSpec) -> TokenStream {
+    let var_ident = field.ident().clone();
+    let field_key = syn::LitStr::new(&field.ident().to_string(), Span::call_site());
+
+    quote! {
+        let #var_ident = attr_args[#field_key].unwrap().try_into().unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::field_spec::FieldSpec;
+
+    #[test]
+    fn build_extractor_test() {
+        let ident = syn::parse_quote!(foo);
+        let ty = syn::parse_quote!(bool);
+        let field = FieldSpec::new(&ident, &ty);
+        
+        let extractor = build_extractor(&field);
+        let expected = quote! {
+            let foo = attr_args["foo"].unwrap().try_into().unwrap();
+        };
+
+        assert_eq!(extractor.to_string(), expected.to_string());
+    }
+}
