e4d535ae3ad0d51da54aae3220a51d08b9458e92
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-07-11 21:14:32 -0400

    First draft of the final macro output

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@
 
 extern crate proc_macro;
 
-mod field_spec;
 mod attr_map;
+mod conversion;
+mod extraction;
+mod field_spec;
 
 use std::convert::TryFrom;
 use field_spec::FieldSpec;
@@ -40,34 +42,22 @@ fn extract_fields(input_struct: &syn::ItemStruct) -> Vec<FieldSpec> {
     }
 }
 
-fn impl_parse(input_attr: &AttrMap, struct_name: &syn::Ident, fields: &Vec<FieldSpec>) -> TokenStream {
+fn impl_parse(_input_attr: &AttrMap, struct_name: &syn::Ident, fields: &Vec<FieldSpec>) -> TokenStream {
+    let extractors = fields.iter().map(extraction::build_extractor);
+
+    let extraction = quote! {
+        #(#extractors)*
+    };
+    
     let idents = fields.iter().map(FieldSpec::ident);
-    let tys = fields.iter().map(FieldSpec::ty);
-
-    let inital_field_declarations = generate_field_decl(idents.clone(), tys.clone());
-    let struct_return = generate_struct_return(&struct_name, idents.clone());
-
-    quote! {
-        // Declare each field for the struct
-        #inital_field_declarations
-
-        // Return the struct
-        #struct_return
-    }
-}
-
-fn generate_field_decl<'a, 'b>(idents: impl Iterator<Item = &'a syn::Ident>, tys: impl Iterator<Item = &'b syn::Type>) -> TokenStream {
-    quote! {
-        #(let mut #idents : Option<#tys> = None;)*
-    }
-}
-
-fn generate_struct_return<'a, 'b>(struct_name: &syn::Ident, idents: impl Iterator<Item = &'a syn::Ident> + Clone) -> TokenStream {
-    let struct_return_idents_1 = idents.clone();
-    let struct_return_idents_2 = idents.clone();
-    quote! {
-        Ok( #struct_name {
-            #(#struct_return_idents_1: #struct_return_idents_2.unwrap()),*
+    let struct_return = quote! {
+        Ok(#struct_name {
+            #(#idents),*
         })
+    };
+
+    quote! {
+        #extraction
+        #struct_return
     }
 }
