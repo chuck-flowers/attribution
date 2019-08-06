a417edc19e0ae8722697fe65596c6b75cf7634ee
Timothy Flowers <tflowers@CHUCKS-DEV-PC.localdomain>
2019-08-06 19:59:42 -0400

    Exports DynamicParameters

--- attribution/src/lib.rs
+++ attribution/src/lib.rs
@@ -6,5 +6,6 @@ mod params;
 pub use attribution_macros::attr_args;
 pub use conversion::FromParameters;
 pub use conversion::FromParametersError;
+pub use params::DynamicParameters;
 pub use params::ParamVal;
 pub use params::Parameters;
