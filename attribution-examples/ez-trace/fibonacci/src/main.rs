640731f89ba40e9d09ab4f90dcd5093abea54c2f
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-12-28 23:19:36 -0500

    Implements unnamed parameters

--- attribution-examples/ez-trace/fibonacci/src/main.rs
+++ attribution-examples/ez-trace/fibonacci/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Finished");
 }
 
-#[ez_trace]
+#[ez_trace("{name}({args})", "{name}({args}) -> {result}")]
 fn fibonacci(n: u32) -> u32 {
     if n == 0 || n == 1 {
         n
