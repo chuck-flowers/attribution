200ccf022c592504e92a7ab6d6f95ae7e3c4dcf6
Timothy Flowers <t_chuck_flowers@yahoo.com>
2020-06-10 07:56:07 -0400

    Adds an impl for Display to ParamKey

--- attribution/src/params/keys.rs
+++ attribution/src/params/keys.rs
@@ -1,8 +1,21 @@
+use core::fmt::Display;
+use core::fmt::Formatter;
+use core::fmt::Result as FmtResult;
+
 /// Represents an identifier for a `ParamVal`
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum ParamKey {
     Named(String),
     Unnamed(usize),
+}
+
+impl Display for ParamKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            ParamKey::Named(name) => name.fmt(f),
+            ParamKey::Unnamed(pos) => pos.fmt(f),
+        }
+    }
 }
 
 impl From<String> for ParamKey {
